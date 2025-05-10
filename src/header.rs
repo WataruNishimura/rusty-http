57986f848ce8d96264d07d2aaf70da0c19444791
Wataru Nishimura <wataru.nishimura@socialchangelab.jp>
2025-05-10 19:28:30 +0900

    feat: 一部未実装だったため追加実装

--- src/header.rs
+++ src/header.rs
@@ -5,5 +5,12 @@ pub struct Header {
 impl Header {
     pub fn new(str: String) -> Self {
         let mut parts = str.splitn(2, ": ");
+        let name = parts.next().unwrap_or("").to_string();
+        let value = parts.next().unwrap_or("").to_string();
+
+        Self {
+            name,
+            value,
+        }
     }
 }
