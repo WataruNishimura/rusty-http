070257bd20d49f94ba6c01ea1fc20af4a8d068e6
Wataru Nishimura <wataru.nishimura@socialchangelab.jp>
2025-05-10 19:21:36 +0900

    feat: error構造体をclineに作らせた

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,24 @@
-pub struct Error {}
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Error {
+    Network(String),
+    // We can add other error variants here later
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Network(msg) => write!(f, "Network error: {}", msg),
+        }
+    }
+}
+
+impl std::error::Error for Error {} // So it can be used with `Box<dyn std::error::Error>` etc.
+
+// We can implement the constructor here
+impl Error {
+    pub fn network(message: String) -> Self {
+        Error::Network(message)
+    }
+}
